01c289b382abad1a5430fb697297d833f51f5adf
Afnan-Sultan-Trino <asultantrino@gmail.com>
2026-03-13 00:25:36 +0600

    Video Processing using opencv

--- Basics.py
+++ Basics.py
@@ -5,3 +5,19 @@ import cv2
 img=cv2.imread("Resources/Screenshot 2026-03-12 225422.png")
 
 print(img)
+print(img.shape)
+cv2.imshow("Batman",img)
+cv2.waitKey(0)
+
+##Reading videos
+
+cap=cv2.VideoCapture("Resources/6994190_Cyborg_Generative_Ai_3840x2160.mp4")
+
+print(cap)
+
+while True:
+    success,frame=cap.read()
+    print(frame.shape)
+    cv2.imshow("Output",frame)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+     break 
